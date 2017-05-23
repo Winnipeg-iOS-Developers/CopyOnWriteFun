f929013d7ff71f0f7bad5433f4ee3f92c67775db
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-05-22 19:09:58 -0500

    Using Assign and Mutate Next buttons to demonstrate copy-on-write by watching memory usage increase as we mutate each of five string vars.

--- CopyOnWriteFun/ViewController.swift
+++ CopyOnWriteFun/ViewController.swift
@@ -1,25 +1,48 @@
-//
-//  ViewController.swift
-//  CopyOnWriteFun
-//
-//  Created by Jeffrey Fulton on 2017-05-19.
-//  Copyright © 2017 Jeffrey Fulton. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // MARK: - Stored Properties
+    
+    var firstString: String = {
+        let image: UIImage = #imageLiteral(resourceName: "Billie")
+        let data = UIImageJPEGRepresentation(image, 1.0)!
+        return data.base64EncodedString()
+    }()
+    
+    var secondString: String?
+    var thirdString: String?
+    var fourthString: String?
+    var fifthString: String?
+    
+    // MARK: - Actions
+    
+    @IBAction func assignTapped(_ sender: Any) {
+        secondString = firstString
+        thirdString = firstString
+        fourthString = firstString
+        fifthString = firstString
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var nextStringToMutateIndex = 2
+    
+    @IBAction func mutateNextTapped(_ sender: Any) {
+        switch nextStringToMutateIndex {
+        case 1: firstString.append("")
+        case 2: secondString?.append("")
+        case 3: thirdString?.append("")
+        case 4: fourthString?.append("")
+        case 5: fifthString?.append("")
+        default: break
+        }
+        
+        if nextStringToMutateIndex < 5 {
+            // Move to the next string
+            nextStringToMutateIndex += 1
+        } else {
+            // Start at the first string.
+            nextStringToMutateIndex = 1
+        }
     }
-
-
 }
 
